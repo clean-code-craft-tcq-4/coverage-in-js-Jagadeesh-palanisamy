9d160c6cc2d1dd7ae2483d7a5d9414567e9ac968
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-18 20:02:31 +0530

    Update typewise-alert.test.js

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -1,10 +1,10 @@
 const alerts = require('../typewise-alert');
 const {expect} = require('chai');
-expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',33)).equals('TOO_LOW');
+expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',13)).equals('TOO_LOW');
 expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',35)).equals('NORMAL');
 expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',40)).equals('TOO_HIGH');
 
-expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',33)).equals('TOO_LOW');
+expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',13)).equals('TOO_LOW');
 expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',35)).equals('NORMAL');
 expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',40)).equals('TOO_HIGH');
 
