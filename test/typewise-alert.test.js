d91ebdefcce884d2f3a60743acc8f03e4636e6ee
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-18 12:55:14 +0530

    Update typewise-alert.test.js

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -2,5 +2,5 @@ const alerts = require('../typewise-alert');
 const {expect} = require('chai');
 
 it('infers a value lower than the minimum as TOO_LOW', () => {
-  expect(alerts.inferBreach(20, 50, 100)).equals('TOO_LOW');
+  expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',50)).equals('65261 TOO_HIGH');
 });
