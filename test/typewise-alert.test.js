82e3b355b05bd2aabbb43e4852937eca2bd04ac5
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-18 20:06:17 +0530

    Update typewise-alert.test.js

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -1,11 +1,11 @@
 const alerts = require('../typewise-alert');
 const {expect} = require('chai');
-expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',13)).equals('TOO_LOW');
-expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',35)).equals('NORMAL');
+expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',-13)).equals('TOO_LOW');
+expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',33)).equals('NORMAL');
 expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',40)).equals('TOO_HIGH');
 
-expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',13)).equals('TOO_LOW');
-expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',35)).equals('NORMAL');
+expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',-13)).equals('TOO_LOW');
+expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',33)).equals('NORMAL');
 expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',40)).equals('TOO_HIGH');
 
 // expect(alerts.checkAndAlert('TO_EMAIL','HI_ACTIVE_COOLING',50)).equals('TOO_HIGH');
