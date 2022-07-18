2a5fc1fcef0aee41c2b31c9eccc49f3f2510b3d7
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-18 19:07:09 +0530

    Update classifyBreach.js

--- classifyBreach.js
+++ classifyBreach.js
@@ -9,14 +9,17 @@ function inferBreach(value, lowerLimit, upperLimit) {
 }
 function classifyTemperatureBreach(coolingType, temperatureInC) {
   let breachType='';
-   if(coolingType==='PASSIVE_COOLING')
-breachType = passiveCooling(coolingType,temperatureInC)
-//  if(coolingType==='HI_ACTIVE_COOLING')
-// breachType = highActiveCooling(coolingType,temperatureInC)
-//  if(coolingType==='MED_ACTIVE_COOLING')
-// breachType = medActiveCooling(coolingType,temperatureInC)
-// console.log(breachType)
+if(coolingType==='PASSIVE_COOLING'){
+ breachType =  passiveCooling();
+  }
+if(coolingType==='HI_ACTIVE_COOLING'){
+ breachType = highActiveCooling();
+}
+if(coolingType==='MED_ACTIVE_COOLING'){
+  breachType = medActiveCooling();
+}
 return breachType;
+
 }
 function passiveCooling(coolingType,temperatureInC){
   return inferBreach(temperatureInC, 0, 35);
@@ -25,6 +28,6 @@ function highActiveCooling(coolingType,temperatureInC){
      return inferBreach(temperatureInC, 0, 45);
 }
 function medActiveCooling(coolingType,temperatureInC){
-return inferBreach(temperatureInC, 0, 40); 
+    return inferBreach(temperatureInC, 0, 40); 
  }
 module.exports ={classifyTemperatureBreach}
