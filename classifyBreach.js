a2ddcfc127073a0a6268ba87f88a447f0877c11b
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-19 21:54:49 +0530

    Update classifyBreach.js

--- classifyBreach.js
+++ classifyBreach.js
@@ -14,4 +14,4 @@ function classifyTemperatureBreach(coolingType, temperatureInC) {
   breachType = coolType[coolingType];
   return inferBreach(temperatureInC,breachType[0], breachType[1]);
 }
-module.exports ={classifyTemperatureBreach}
+module.exports ={classifyTemperatureBreach,classifyInferBreach}
