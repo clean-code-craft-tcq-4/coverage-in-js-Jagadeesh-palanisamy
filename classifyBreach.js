4bb82f4a2dcd40a04de7469b2a2fc0f76cdab7ce
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-19 18:18:02 +0530

    Update classifyBreach.js

--- classifyBreach.js
+++ classifyBreach.js
@@ -1,10 +1,4 @@
-const {coolingTypeLimit} = require('./enum');
-const coolType = {
-  "PASSIVE_COOLING":[0,35],
-  "HI_ACTIVE_COOLING":[0,45],
-  "MED_ACTIVE_COOLING":[0,40]
-}
-
+const {coolType} = require('./enum');
 function inferBreach(value, lowerLimit, upperLimit) {
   if (value < lowerLimit) {
     return 'TOO_LOW';
@@ -17,7 +11,7 @@ function inferBreach(value, lowerLimit, upperLimit) {
 
 function classifyTemperatureBreach(coolingType, temperatureInC) {
   let breachType=[];
-  breachType = coolingTypeLimit[coolingType];
+  breachType = coolType[coolingType];
   return inferBreach(temperatureInC,breachType[0], breachType[1]);
 }
 module.exports ={classifyTemperatureBreach}
