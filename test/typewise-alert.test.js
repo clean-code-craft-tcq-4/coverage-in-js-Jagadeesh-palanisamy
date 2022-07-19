106ed66b45f7913fec4b1438f02de241d6e2d2c6
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-19 22:37:44 +0530

    Update typewise-alert.test.js

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -27,9 +27,11 @@ expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',33)).equals('NORMAL'
 expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',42)).equals('TOO_HIGH');
 
 });
-it('classifyInferBreach', () => {
-expect(classifyInferBreach.inferBreach(-10,0,35)).equals('TOO_LOW');
-  expect(classifyInferBreach.inferBreach(33,0,35)).equals('NORMAL');
-  expect(classifyInferBreach.inferBreach(50,0,35)).equals('TOO_HIGH');
+it('tooLowEmail', () => {
+expect(alerts.tooLowEmail('To: a.b@c.com')).equals('Hi, the temperature is too low');
 });
+it('tooHighEmail', () => {
+expect(alerts.tooLowEmail('To: a.b@c.com')).equals('Hi, the temperature is too high');
+});
+
 
