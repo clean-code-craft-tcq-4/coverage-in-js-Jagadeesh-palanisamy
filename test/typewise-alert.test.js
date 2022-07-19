8355d25ff21ce54a69b762e90d64f7978317ad21
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-19 21:48:51 +0530

    Update typewise-alert.test.js

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -1,5 +1,6 @@
 const alerts = require('../typewise-alert');
 const {expect} = require('chai');
+it('checkAndAlert', () => {
 expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',-13)).equals('TOO_LOW');
 expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',33)).equals('NORMAL');
 expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',40)).equals('TOO_HIGH');
@@ -24,6 +25,10 @@ expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',-13)).equals('TOO_LO
 expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',33)).equals('NORMAL');
 expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',42)).equals('TOO_HIGH');
 
-
-// expect(alerts.checkAndAlert('TO_EMAIL','HI_ACTIVE_COOLING',50)).equals('TOO_HIGH');
+});
+it('checkAndAlert', () => {
+expect(alerts.inferBreach(10,0,35)).equals('TOO_LOW');
+  expect(alerts.inferBreach(33,0,35)).equals('NORMAL');
+  expect(alerts.inferBreach(50,0,35)).equals('TOO_HIGH');
+});
 
