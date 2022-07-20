262320760d97473ded1e85df66dd6e728500a1b5
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-20 12:08:12 +0530

    Update typewise-alert.js

--- typewise-alert.js
+++ typewise-alert.js
@@ -1,44 +1,47 @@
 const breach = require('./classifyBreach');
 function checkAndAlert(alertTarget, batteryChar, temperatureInC) {
-const breachType= breach.classifyTemperatureBreach(batteryChar, temperatureInC)
-   console.log(breachType)
-   if (alertTarget == 'TO_CONTROLLER') {
-    sendToController(breachType);
-  } else if (alertTarget == 'TO_EMAIL') {
-    sendToEmail(breachType);
-  }
-return breachType;
+    const breachType = breach.classifyTemperatureBreach(batteryChar, temperatureInC)
+    return alertAction(breachType, alertTarget);
 }
-
+function alertAction(breachType, alertTarget) {
+    if (alertTarget == 'TO_CONTROLLER') {
+        return sendToController(breachType);
+    }
+    if (alertTarget == 'TO_EMAIL') {
+        return sendToEmail(breachType);
+    }
+    return false
+}
 
 function sendToController(breachType) {
-  const header = 0xfeed;
-  const toPrint =`${header}`+' '+breachType;
-  printStatement(toPrint);
+    const header = 0xfeed;
+    const toPrint = `${header}` + ' ' + breachType;
+    printStatement(toPrint);
+    return true;
 }
 
-function printStatement(statement){  
-  console.log(statement);
-  }
 
-  
-  function sendToEmail(breachType) {
+function printStatement(statement) {
+    console.log(statement);
+}
+
+function sendToEmail(breachType) {
     const recepient = 'a.b@c.com';
-    const toEmail = 'To: '+recepient;
+    const toEmail = 'To: ' + recepient;
     if (breachType == 'TOO_LOW')
-  tooLowEmail(toEmail);
+        return tooLowEmail(toEmail);
     if (breachType == 'TOO_HIGH')
-   tooHighEmail(toEmail);
+        return tooHighEmail(toEmail);
+    return false;
 }
-
-function tooLowEmail(toEmail){
-  printStatement(toEmail);
-printStatement('Hi, the temperature is too low');
-  }
-function tooHighEmail(toEmail){
-   printStatement(toEmail);
-  printStatement('Hi, the temperature is too high');
+function tooLowEmail(toEmail) {
+    printStatement(toEmail);
+    printStatement('Hi, the temperature is too low');
+    return true;
 }
-
-
-module.exports ={checkAndAlert};
+function tooHighEmail(toEmail) {
+    printStatement(toEmail);
+    printStatement('Hi, the temperature is too high');
+    return true;
+}
+module.exports = { checkAndAlert };
