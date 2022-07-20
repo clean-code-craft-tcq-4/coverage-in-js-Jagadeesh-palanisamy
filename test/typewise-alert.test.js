96a6195b2ab7ac0553fbd3f70ac17746f061eb85
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-07-20 12:13:28 +0530

    Update typewise-alert.test.js

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -27,29 +27,7 @@ expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',-13));
 expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',33));
 expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',42));
   
-// expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',-13)).equals('TOO_LOW');
-// expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',33)).equals('NORMAL');
-// expect(alerts.checkAndAlert('TO_CONTROLLER','PASSIVE_COOLING',40)).equals('TOO_HIGH');
 
-// expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',-13)).equals('TOO_LOW');
-// expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',33)).equals('NORMAL');
-// expect(alerts.checkAndAlert('TO_EMAIL','PASSIVE_COOLING',40)).equals('TOO_HIGH');
-
-// expect(alerts.checkAndAlert('TO_CONTROLLER','HI_ACTIVE_COOLING',-13)).equals('TOO_LOW');
-// expect(alerts.checkAndAlert('TO_CONTROLLER','HI_ACTIVE_COOLING',33)).equals('NORMAL');
-// expect(alerts.checkAndAlert('TO_CONTROLLER','HI_ACTIVE_COOLING',50)).equals('TOO_HIGH');
-
-// expect(alerts.checkAndAlert('TO_EMAIL','HI_ACTIVE_COOLING',-13)).equals('TOO_LOW');
-// expect(alerts.checkAndAlert('TO_EMAIL','HI_ACTIVE_COOLING',33)).equals('NORMAL');
-// expect(alerts.checkAndAlert('TO_EMAIL','HI_ACTIVE_COOLING',50)).equals('TOO_HIGH');
-
-// expect(alerts.checkAndAlert('TO_CONTROLLER','MED_ACTIVE_COOLING',-13)).equals('TOO_LOW');
-// expect(alerts.checkAndAlert('TO_CONTROLLER','MED_ACTIVE_COOLING',33)).equals('NORMAL');
-// expect(alerts.checkAndAlert('TO_CONTROLLER','MED_ACTIVE_COOLING',42)).equals('TOO_HIGH');
-
-// expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',-13)).equals('TOO_LOW');
-// expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',33)).equals('NORMAL');
-// expect(alerts.checkAndAlert('TO_EMAIL','MED_ACTIVE_COOLING',42)).equals('TOO_HIGH');
 
 });
 
